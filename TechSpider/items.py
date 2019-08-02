16faf3505aa210b4c458f336c0f928615c327843
Shunqi Wang <wangshunqi2@gmail.com>
2019-08-01 23:30:14 -0500

    Create item and item loader for articles

--- TechSpider/items.py
+++ TechSpider/items.py
@@ -6,9 +6,24 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst
 
 
 class TechspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class ArticleItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
+class TechCrunchArticleItem(scrapy.Item):
+    title = scrapy.Field()
+    create_date = scrapy.Field()
+    url = scrapy.Field()
+    url_object_id = scrapy.Field()
+    front_image_url = scrapy.Field()
+    front_image_path = scrapy.Field()
+    tags = scrapy.Field()
+    content = scrapy.Field()
